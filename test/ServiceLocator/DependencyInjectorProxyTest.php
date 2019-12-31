f12845bc5b95c0c298e7f9db4040c41f037c12c4
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:16:28 -0600

    Rewriting as Laminas Project package

--- test/ServiceLocator/DependencyInjectorProxyTest.php
+++ test/ServiceLocator/DependencyInjectorProxyTest.php
@@ -1,17 +1,16 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-di for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-di/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-di/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Di\ServiceLocator;
+namespace LaminasTest\Di\ServiceLocator;
 
-use Zend\Di\Di;
-use Zend\Di\ServiceLocator\DependencyInjectorProxy;
-use ZendTest\Di\TestAsset\SetterInjection\A;
+use Laminas\Di\Di;
+use Laminas\Di\ServiceLocator\DependencyInjectorProxy;
+use LaminasTest\Di\TestAsset\SetterInjection\A;
 use PHPUnit_Framework_TestCase as TestCase;
 
 /**
@@ -24,11 +23,11 @@ class DependencyInjectorProxyTest extends TestCase
         $di = new Di();
         $a = new A();
         $di->instanceManager()->setParameters(
-            'ZendTest\Di\TestAsset\SetterInjection\B',
+            'LaminasTest\Di\TestAsset\SetterInjection\B',
             array('a' => $a)
         );
         $proxy = new DependencyInjectorProxy($di);
-        $b = $proxy->get('ZendTest\Di\TestAsset\SetterInjection\B');
+        $b = $proxy->get('LaminasTest\Di\TestAsset\SetterInjection\B');
         $methods = $b->getMethods();
         $this->assertSame('setA', $methods[0]['method']);
         $this->assertSame($a, $methods[0]['params'][0]);
