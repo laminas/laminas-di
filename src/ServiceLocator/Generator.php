cc7886773ee7b07ed627a6b8ff776b82e37dd471
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:16:09 -0600

    Rewriting as Laminas Project package

--- src/ServiceLocator/Generator.php
+++ src/ServiceLocator/Generator.php
@@ -1,23 +1,22 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-di for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-di/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-di/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Di\ServiceLocator;
+namespace Laminas\Di\ServiceLocator;
 
-use Zend\Code\Generator\ClassGenerator;
-use Zend\Code\Generator\FileGenerator;
-use Zend\Code\Generator\MethodGenerator;
-use Zend\Code\Generator\ParameterGenerator;
-use Zend\Di\Di;
-use Zend\Di\Exception;
+use Laminas\Code\Generator\ClassGenerator;
+use Laminas\Code\Generator\FileGenerator;
+use Laminas\Code\Generator\MethodGenerator;
+use Laminas\Code\Generator\ParameterGenerator;
+use Laminas\Di\Di;
+use Laminas\Di\Exception;
 
 /**
- * Generator that creates the body of a service locator that can emulate the logic of the given Zend\Di\Di instance
+ * Generator that creates the body of a service locator that can emulate the logic of the given Laminas\Di\Di instance
  * without class definitions
  */
 class Generator
@@ -73,11 +72,11 @@ class Generator
     /**
      * Construct, configure, and return a PHP class file code generation object
      *
-     * Creates a Zend\Code\Generator\FileGenerator object that has
+     * Creates a Laminas\Code\Generator\FileGenerator object that has
      * created the specified class and service locator methods.
      *
      * @param  null|string                         $filename
-     * @throws \Zend\Di\Exception\RuntimeException
+     * @throws \Laminas\Di\Exception\RuntimeException
      * @return FileGenerator
      */
     public function getCodeGenerator($filename = null)
@@ -266,7 +265,7 @@ class Generator
 
         // Create PHP file code generation object
         $classFile = new FileGenerator();
-        $classFile->setUse('Zend\Di\ServiceLocator')
+        $classFile->setUse('Laminas\Di\ServiceLocator')
                   ->setClass($container);
 
         if (null !== $this->namespace) {
