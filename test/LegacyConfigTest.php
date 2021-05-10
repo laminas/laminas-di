72f318e4236afc70c2b46d20d6b6c0af711c8b7b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:06:15 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/LegacyConfigTest.php
+++ test/LegacyConfigTest.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-di for the canonical source repository
- * @copyright https://github.com/laminas/laminas-di/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-di/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace LaminasTest\Di;
--- test/TestAsset/Constructor/ManyArguments.php
+++ test/TestAsset/Constructor/ManyArguments.php
@@ -2,12 +2,6 @@
 
 // @phpcs:disable
 
-/**
- * @see       https://github.com/laminas/laminas-di for the canonical source repository
- * @copyright https://github.com/laminas/laminas-di/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-di/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace LaminasTest\Di\TestAsset\Constructor;
