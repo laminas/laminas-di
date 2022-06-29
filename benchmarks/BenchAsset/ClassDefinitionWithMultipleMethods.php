53def9263996bed8987b9c69cdcdfe6256dacd6e
Remy Bos <27890746+sjokkateer@users.noreply.github.com>
2022-06-29 20:24:54 +0200

    CLASS DEFINITION: Add additional benchmark tests for getParameters
    Signed-off-by: Remy Bos <27890746+sjokkateer@users.noreply.github.com>

---
+++ benchmarks/BenchAsset/ClassDefinitionWithMultipleMethods.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaminasBench\Di\BenchAsset;
+
+class ClassDefinitionWithMultipleMethods
+{
+    public function __construct()
+    {
+    }
+
+    public function methodOne(): void
+    {
+    }
+
+    public function methodTwo(): void
+    {
+    }
+
+    public function methodThree(): void
+    {
+    }
+
+    public function methodFour(): void
+    {
+    }
+
+    public function methodFive(): void
+    {
+    }
+
+    public function methodSix(): void
+    {
+    }
+
+    public function methodSeven(): void
+    {
+    }
+
+    public function methodEight(): void
+    {
+    }
+
+    public function methodNine(): void
+    {
+    }
+
+    public function methodTen(): void
+    {
+    }
+
+    public function methodEleven(): void
+    {
+    }
+
+    public function methodTwelve(): void
+    {
+    }
+}
--- benchmarks/ClassDefinitionBench.php
+++ benchmarks/ClassDefinitionBench.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace LaminasBench\Di;
 
 use Laminas\Di\Definition\Reflection\ClassDefinition;
+use LaminasBench\Di\BenchAsset\ClassDefinitionWithConstructorInherited;
 use LaminasBench\Di\BenchAsset\ClassDefinitionWithMultipleConstructorParameters;
+use LaminasBench\Di\BenchAsset\ClassDefinitionWithMultipleMethods;
+use LaminasBench\Di\BenchAsset\ClassDefinitionWithNoOtherMethods;
 use LaminasBench\Di\BenchAsset\ClassDefinitionWithoutConstructor;
 use LaminasBench\Di\BenchAsset\ClassDefinitionWithoutConstructorParameters;
 
@@ -15,11 +18,19 @@ class ClassDefinitionBench
     private ClassDefinition $noConstructorParameters;
     private ClassDefinition $multipleConstructorParameters;
 
+    private ClassDefinition $noOtherMethods;
+    private ClassDefinition $multipleMethods;
+    private ClassDefinition $inheritedConstructor;
+
     public function __construct()
     {
-        $this->noConstructor = new ClassDefinition(ClassDefinitionWithoutConstructor::class);
-        $this->noConstructorParameters = new ClassDefinition(ClassDefinitionWithoutConstructorParameters::class);
+        $this->noConstructor                 = new ClassDefinition(ClassDefinitionWithoutConstructor::class);
+        $this->noConstructorParameters       = new ClassDefinition(ClassDefinitionWithoutConstructorParameters::class);
         $this->multipleConstructorParameters = new ClassDefinition(ClassDefinitionWithMultipleConstructorParameters::class);
+
+        $this->noOtherMethods       = new ClassDefinition(ClassDefinitionWithNoOtherMethods::class);
+        $this->multipleMethods      = new ClassDefinition(ClassDefinitionWithMultipleMethods::class);
+        $this->inheritedConstructor = new ClassDefinition(ClassDefinitionWithConstructorInherited::class);
     }
 
     public function benchGetParametersBeforeTransparentCachingNoConstructor(): void
@@ -41,5 +52,26 @@ class ClassDefinitionBench
         $multipleConstructorParameters = clone $this->multipleConstructorParameters;
 
         $multipleConstructorParameters->getParameters();
+    }
+
+    public function benchGetParametersConstructorNoOtherMethods(): void
+    {
+        $noOtherMethods = clone $this->noOtherMethods;
+
+        $noOtherMethods->getParameters();
+    }
+
+    public function benchGetParametersConstructorMultipleMethods(): void
+    {
+        $multipleMethods = clone $this->multipleMethods;
+
+        $multipleMethods->getParameters();
+    }
+
+    public function benchGetParametersConstructorInheritedNoOtherMethods(): void
+    {
+        $inheritedConstructor = clone $this->inheritedConstructor;
+
+        $inheritedConstructor->getParameters();
     }
 }
